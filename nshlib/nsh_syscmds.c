9e2e15ff56fb913675b76d690dea7f0ec384750f
openvela-robot <openvela-robot@xiaomi.com>
2022-01-23 16:31:43 +0800

    system/adb: Replace printf with syslog since adb is a service
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- nshlib/nsh_syscmds.c
+++ nshlib/nsh_syscmds.c
@@ -57,16 +57,16 @@
 #define UNAME_KERNEL   (1 << 0)
 #define UNAME_NODE     (1 << 1)
 #define UNAME_RELEASE  (1 << 2)
-#define UNAME_VERSION  (1 << 3)
+#define UNAME_VERISON  (1 << 3)
 #define UNAME_MACHINE  (1 << 4)
 #define UNAME_PLATFORM (1 << 5)
 #define UNAME_UNKNOWN  (1 << 6)
 
 #ifdef CONFIG_NET
 #  define UNAME_ALL    (UNAME_KERNEL | UNAME_NODE | UNAME_RELEASE | \
-                        UNAME_VERSION | UNAME_MACHINE | UNAME_PLATFORM)
+                        UNAME_VERISON | UNAME_MACHINE | UNAME_PLATFORM)
 #else
-#  define UNAME_ALL    (UNAME_KERNEL | UNAME_RELEASE | UNAME_VERSION | \
+#  define UNAME_ALL    (UNAME_KERNEL | UNAME_RELEASE | UNAME_VERISON | \
                         UNAME_MACHINE | UNAME_PLATFORM)
 #endif
 
@@ -112,7 +112,7 @@ int cmd_shutdown(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
        * reset the board due to some constraints.
        */
 
-      boardctl(BOARDIOC_RESET, 0);
+      boardctl(BOARDIOC_RESET, EXIT_SUCCESS);
     }
   else
     {
@@ -121,7 +121,7 @@ int cmd_shutdown(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
        * to power-off the* board due to some constraints.
        */
 
-      boardctl(BOARDIOC_POWEROFF, 0);
+      boardctl(BOARDIOC_POWEROFF, EXIT_SUCCESS);
     }
 
 #elif defined(CONFIG_BOARDCTL_RESET)
@@ -142,7 +142,7 @@ int cmd_shutdown(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
    * reset the board due to some constraints.
    */
 
-  boardctl(BOARDIOC_RESET, 0);
+  boardctl(BOARDIOC_RESET, EXIT_SUCCESS);
 
 #else
   /* Only the reset behavior is supported and we already know that there is
@@ -154,10 +154,10 @@ int cmd_shutdown(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
    * off the board due to some constraints.
    */
 
-  boardctl(BOARDIOC_POWEROFF, 0);
+  boardctl(BOARDIOC_POWEROFF, EXIT_SUCCESS);
 #endif
 
-  /* boardctl() will not return in any case.  It if does, it means that
+  /* boarctl() will not return in any case.  It if does, it means that
    * there was a problem with the shutdown/resaet operation.
    */
 
@@ -277,10 +277,10 @@ int cmd_poweroff(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
     }
   else
     {
-      boardctl(BOARDIOC_POWEROFF, 0);
+      boardctl(BOARDIOC_POWEROFF, EXIT_SUCCESS);
     }
 
-  /* boardctl() will not return in any case.  It if does, it means that
+  /* boarctl() will not return in any case.  It if does, it means that
    * there was a problem with the shutdown operation.
    */
 
@@ -307,10 +307,10 @@ int cmd_reboot(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
     }
   else
     {
-      boardctl(BOARDIOC_RESET, 0);
+      boardctl(BOARDIOC_RESET, EXIT_SUCCESS);
     }
 
-  /* boardctl() will not return in this case.  It if does, it means that
+  /* boarctl() will not return in this case.  It if does, it means that
    * there was a problem with the reset operation.
    */
 
@@ -326,6 +326,7 @@ int cmd_reboot(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
 #if defined(CONFIG_RPTUN) && !defined(CONFIG_NSH_DISABLE_RPTUN)
 int cmd_rptun(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
 {
+  unsigned long val = 0;
   int fd;
   int cmd;
 
@@ -343,6 +344,19 @@ int cmd_rptun(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
     {
       cmd = RPTUNIOC_STOP;
     }
+  else if (strcmp(argv[1], "reset") == 0)
+    {
+      if (argc > 3)
+        {
+          val = atoi(argv[3]);
+        }
+
+      cmd = RPTUNIOC_RESET;
+    }
+  else if (strcmp(argv[1], "panic") == 0)
+    {
+      cmd = RPTUNIOC_PANIC;
+    }
   else
     {
       nsh_output(vtbl, g_fmtarginvalid, argv[1]);
@@ -356,7 +370,7 @@ int cmd_rptun(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
       return ERROR;
     }
 
-  ioctl(fd, cmd, 0);
+  ioctl(fd, cmd, val);
 
   close(fd);
   return 0;
@@ -408,7 +422,7 @@ int cmd_uname(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
             break;
 
           case 'v':
-            set |= UNAME_VERSION;
+            set |= UNAME_VERISON;
             break;
 
           case 'm':
