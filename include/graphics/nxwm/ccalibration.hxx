4ac3b26f24b81c23824673022b7f6e0650e416ec
openvela-robot <openvela-robot@xiaomi.com>
2022-11-04 18:36:00 +0800

    Fix slcd_trapezoids.cxx:38:5: error: array designators are a C99 extension
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- include/graphics/nxwm/ccalibration.hxx
+++ include/graphics/nxwm/ccalibration.hxx
@@ -64,7 +64,7 @@ namespace NxWM
    * Forward references
    */
 
-  class CTouchscreen;
+  struct CTouchscreen;
 
   /**
    * Touchscreen calibration data
@@ -164,6 +164,7 @@ namespace NxWM
     struct nxgl_point_s        m_touchPos;        /**< This is the last touch position */
     volatile uint8_t           m_calthread;       /**< Current calibration display state (See ECalibThreadState)*/
     uint8_t                    m_calphase;        /**< Current calibration display state (See ECalibrationPhase)*/
+    bool                       m_stop;            /**< True: We have been asked to stop the calibration */
     bool                       m_touched;         /**< True: The screen is touched */
     uint8_t                    m_touchId;         /**< The ID of the touch */
 #ifdef CONFIG_NXWM_CALIBRATION_AVERAGE
