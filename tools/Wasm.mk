342a937bcea842d2a1bfbdd130dd729e61ec5b59
openvela-robot <openvela-robot@xiaomi.com>
2023-06-02 08:55:23 +0800

    system/nxdiag: Fix issues introduced by #1776
    Fix compilation issue by properly providing the defconfig name

--- tools/Wasm.mk
+++ tools/Wasm.mk
@@ -30,7 +30,7 @@ WAR ?= $(WASI_SDK_PATH)/bin/llvm-ar rcs
 
 ifeq ($(WSYSROOT),)
 	WSYSROOT := $(TOPDIR)
-
+	
 	# Force disable wasm build when WASM_SYSROOT is not defined and on specific
 	# targets that do not support wasm build.
 	# Since some architecture level inline assembly instructions can not be
@@ -108,20 +108,20 @@ WAMR_MODE ?= INT
 .PRECIOUS: $(WBIN)
 
 WSRCS := $(MAINSRC) $(CSRCS)
-WOBJS := $(WSRCS:%.c=%.wo)
+WOBJS := $(WSRCS:=$(SUFFIX).wo)
 
 all:: $(WBIN)
 
-$(WOBJS): %.wo : %.c
+$(WOBJS): %.c$(SUFFIX).wo : %.c
 	$(Q) $(WCC) $(WCFLAGS) -c $^ -o $@
 
 $(WBIN): $(WOBJS)
 	$(shell mkdir -p $(APPDIR)/wasm)
-	$(Q) $(WAR) $@ $(filter-out $(MAINSRC:%.c=%.wo),$^)
+	$(Q) $(WAR) $@ $(filter-out $(MAINSRC:=$(SUFFIX).wo),$^)
 	$(foreach main,$(MAINSRC), \
 	  $(eval mainindex=$(strip $(call GETINDEX,$(main),$(MAINSRC)))) \
-	  $(eval dstname=$(shell echo $(main:%.c=%.wo) | sed -e 's/\//_/g')) \
-	  $(shell cp -rf $(strip $(main:%.c=%.wo)) \
+	$(eval dstname=$(shell echo $(main:=$(SUFFIX).wo) | sed -e 's/\//_/g')) \
+	  $(shell cp -rf $(strip $(main:=$(SUFFIX).wo)) \
 	    $(strip $(APPDIR)/wasm/$(word $(mainindex),$(PROGNAME))#$(WASM_INITIAL_MEMORY)#$(STACKSIZE)#$(PRIORITY)#$(WAMR_MODE)#$(dstname)) \
 	   ) \
 	 )
