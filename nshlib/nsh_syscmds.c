c28145e662c1782d4e0ffd4607aa901d9e983995
openvela-robot <openvela-robot@xiaomi.com>
2024-01-26 11:04:33 +0800

    nsh_timcmds: display date using set format
    Reference: https://www.geeksforgeeks.org/date-command-linux-examples/?ref=gcse#9-list-of-format-specifiers-used-with-date-command
    
    Signed-off-by: yangjian11 <yangjian11@xiaomi.com>

--- nshlib/nsh_syscmds.c
+++ nshlib/nsh_syscmds.c
@@ -117,7 +117,6 @@ static FAR const char * const g_resetflag[] =
   "panic",
   "bootloader",
   "recovery",
-  "restore",
   "factory",
   NULL
 };
@@ -571,7 +570,7 @@ static int cmd_rptun_once(FAR struct nsh_vtbl_s *vtbl,
       return ERROR;
     }
 
-  fd = open(path, O_CLOEXEC);
+  fd = open(path, 0);
   if (fd < 0)
     {
       nsh_output(vtbl, g_fmtarginvalid, path);
@@ -616,6 +615,7 @@ int cmd_rptun(FAR struct nsh_vtbl_s *vtbl, int argc, FAR char **argv)
       nsh_output(vtbl, "  rptun <start|stop|reset|panic|dump> <path> "
                 "<value>\n");
       nsh_output(vtbl, "  rptun <reset> <path> <resetvalue>\n");
+#ifdef CONFIG_RPTUN_PING
       nsh_output(vtbl, "  rptun ping <path> <times> <length> <ack> "
                 "<period(ms)>\n\n");
       nsh_output(vtbl, "  <path>         Rptun device path.\n");
@@ -627,6 +627,7 @@ int cmd_rptun(FAR struct nsh_vtbl_s *vtbl, int argc, FAR char **argv)
       nsh_output(vtbl, "                 1 - Acknowledge, no data check.\n");
       nsh_output(vtbl, "                 2 - Acknowledge and data check.\n");
       nsh_output(vtbl, "  <period(ms)>   ping period (ms) \n\n");
+#endif
 
       return OK;
     }
