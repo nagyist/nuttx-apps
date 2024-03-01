07b455df80c8f16096bb4be3a29e898774a106cd
openvela-robot <openvela-robot@xiaomi.com>
2024-03-01 10:01:15 +0800

    fix patch error:no such file
    Signed-off-by: zhangchao53 <zhangchao53@xiaomi.com>

--- nshlib/nsh_command.c
+++ nshlib/nsh_command.c
@@ -469,8 +469,7 @@ static const struct cmdmap_s g_cmdmap[] =
 #endif
 
 #ifndef CONFIG_NSH_DISABLE_PS
-  CMD_MAP("ps",       cmd_ps,       1, CONFIG_NSH_MAXARGUMENTS,
-    "<pid1 pid2 ...>"),
+  CMD_MAP("ps",       cmd_ps,       1, 1, NULL),
 #endif
 
 #ifdef CONFIG_NET_UDP
