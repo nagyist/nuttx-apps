1106b3818550d108928753b395803135a28baa23
openvela-robot <openvela-robot@xiaomi.com>
2024-04-07 13:28:07 +0800

    canutils: Fix minor style issue found in canlib
    no functional change
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- nshlib/nsh_command.c
+++ nshlib/nsh_command.c
@@ -456,7 +456,6 @@ static const struct cmdmap_s g_cmdmap[] =
 
 #if defined(CONFIG_BOARDCTL_POWEROFF) && !defined(CONFIG_NSH_DISABLE_POWEROFF)
   CMD_MAP("poweroff", cmd_poweroff, 1, 2, NULL),
-  CMD_MAP("quit", cmd_poweroff, 1, 2, NULL),
 #endif
 
 #ifndef CONFIG_NSH_DISABLE_PRINTF
