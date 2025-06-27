ec711bf173eb5a25e6425de0cf37d7f6ab505a1a
wangzhi16 <wangzhi16@xiaomi.com>
2025-06-27 21:51:01 +0800

    testing/setest: fix compile error.
    setest.c:81:18: warning: implicit declaration of function 'getopt' [-Wimplicit-function-declaration]
       81 |   while (((opt = getopt(argc, argv, "b:h")) != -1) && (result == 0))
          |                  ^~~~~~
    setest.c:86:49: error: 'optarg' undeclared (first use in this function)
       86 |           settings->base_id = (uint32_t)strtoul(optarg, NULL, 0);
          |                                                 ^~~~~~
    setest.c:86:49: note: each undeclared identifier is reported only once for each function it appears in
    setest.c:105:11: error: 'optind' undeclared (first use in this function)
      105 |       if (optind != argc)
          |           ^~~~~~
    setest.c: In function 'setest_main':
    setest.c:361:11: warning: implicit declaration of function 'close'; did you mean 'pclose'? [-Wimplicit-function-declaration]
      361 |           close(fd);
          |           ^~~~~
          |           pclose
    
    Signed-off-by: wangzhi16 <wangzhi16@xiaomi.com>

--- testing/setest/setest.c
+++ testing/setest/setest.c
@@ -31,6 +31,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
 
 /****************************************************************************
  * Pre-processor Definitions
