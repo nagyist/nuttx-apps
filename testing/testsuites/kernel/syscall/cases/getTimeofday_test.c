e6a0e8e6a8ded381832e90d43c7ce06b25c9a3f7
huojianchao <huojianchao@xiaomi.com>
2026-01-26 21:31:12 +0800

    testing/syscall: refactor gettimeofday test to handle g_basetime changes
    Use CLOCK_MONOTONIC as reference to compare time deltas instead of
    comparing absolute gettimeofday values. This avoids false failures
    when g_basetime is adjusted by NTP or other time sync services.
    
    - Compare time deltas between gettimeofday and CLOCK_MONOTONIC
    - Allow limited basetime adjustments (max 3) before failing
    - Fail immediately if CLOCK_MONOTONIC goes backwards
    - Fail if gettimeofday delta exceeds CLOCK_MONOTONIC delta
    
    Signed-off-by: huojianchao <huojianchao@xiaomi.com>

--- testing/testsuites/kernel/syscall/cases/getTimeofday_test.c
+++ testing/testsuites/kernel/syscall/cases/getTimeofday_test.c
@@ -23,6 +23,7 @@
  ****************************************************************************/
 #include <nuttx/config.h>
 #include <sys/time.h>
+#include <time.h>
 #include <unistd.h>
 #include <signal.h>
 #include <stdarg.h>
@@ -32,6 +33,16 @@
 #include <setjmp.h>
 #include <cmocka.h>
 #include "SyscallTest.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* Maximum allowed delta difference in microseconds between gettimeofday
+ * and CLOCK_MONOTONIC. This accounts for the time between the two calls.
+ */
+
+#define MAX_DELTA_DIFF_US  50000  /* 50ms tolerance */
 
 /****************************************************************************
  * Private Data
@@ -53,6 +64,24 @@ static void breakout(int sig)
 }
 
 /****************************************************************************
+ * Name: timeval_to_us
+ ****************************************************************************/
+
+static int64_t timeval_to_us(const struct timeval *tv)
+{
+  return (int64_t)tv->tv_sec * 1000000 + tv->tv_usec;
+}
+
+/****************************************************************************
+ * Name: timespec_to_us
+ ****************************************************************************/
+
+static int64_t timespec_to_us(const struct timespec *ts)
+{
+  return (int64_t)ts->tv_sec * 1000000 + ts->tv_nsec / 1000;
+}
+
+/****************************************************************************
  * Public Functions
  ****************************************************************************/
 
@@ -63,8 +92,15 @@ static void breakout(int sig)
 void test_nuttx_syscall_gettimeofday01(FAR void **state)
 {
   int rtime = 1;
+  int basetime_adjust_count = 0;
+  const int max_basetime_adjusts = 3;
   struct timeval tv1;
   struct timeval tv2;
+  struct timespec mono1;
+  struct timespec mono2;
+  int64_t tv_delta;
+  int64_t mono_delta;
+  int64_t delta_diff;
 
   signal(SIGALRM, breakout);
 
@@ -72,21 +108,94 @@ void test_nuttx_syscall_gettimeofday01(FAR void **state)
 
   alarm(rtime);
 
+  /* Get initial timestamps from both sources */
+
   if (gettimeofday(&tv1, NULL))
-    fail_msg("FAIL, gettimeofday() failed\n");
+    {
+      fail_msg("FAIL, gettimeofday() failed\n");
+    }
+
+  if (clock_gettime(CLOCK_MONOTONIC, &mono1))
+    {
+      fail_msg("FAIL, clock_gettime(CLOCK_MONOTONIC) failed\n");
+    }
 
   while (!done)
     {
-      if (gettimeofday(&tv2, NULL))
-        fail_msg("FAIL, gettimeofday() failed\n");
+      /* Get current timestamps from both sources */
 
-      if (tv2.tv_sec < tv1.tv_sec ||
-          (tv2.tv_sec == tv1.tv_sec && tv2.tv_usec < tv1.tv_usec))
+      if (gettimeofday(&tv2, NULL))
         {
-          fail_msg("test fail !");
+          fail_msg("FAIL, gettimeofday() failed\n");
+        }
+
+      if (clock_gettime(CLOCK_MONOTONIC, &mono2))
+        {
+          fail_msg("FAIL, clock_gettime(CLOCK_MONOTONIC) failed\n");
+        }
+
+      /* Calculate deltas - how much time has passed according to each
+       * clock
+       */
+
+      tv_delta = timeval_to_us(&tv2) - timeval_to_us(&tv1);
+      mono_delta = timespec_to_us(&mono2) - timespec_to_us(&mono1);
+
+      /* CLOCK_MONOTONIC should always increase */
+
+      if (mono_delta < 0)
+        {
+          fail_msg("FAIL, CLOCK_MONOTONIC went backwards!\n");
+        }
+
+      /* Compare the deltas. If g_basetime changed, tv_delta will differ
+       * from mono_delta by the amount of the basetime adjustment.
+       * We only fail if the difference is unreasonably large AND
+       * it's not explainable by a basetime change (i.e., mono_delta
+       * is positive but tv_delta is negative without basetime change).
+       */
+
+      delta_diff = tv_delta - mono_delta;
+
+      /* If delta_diff is negative and large, it means gettimeofday
+       * reported less time passed than CLOCK_MONOTONIC.
+       * This is only valid if g_basetime was adjusted backwards.
+       * We allow this but verify the magnitude is reasonable.
+       */
+
+      if (delta_diff < -MAX_DELTA_DIFF_US)
+        {
+          /* Large negative difference - likely g_basetime adjustment.
+           * Allow limited number of adjustments.
+           */
+
+          basetime_adjust_count++;
+          if (basetime_adjust_count > max_basetime_adjusts)
+            {
+              fail_msg("FAIL, too many basetime adjustments (%d), "
+                       "timer may have issues\n", basetime_adjust_count);
+            }
+
+          tv1 = tv2;
+          mono1 = mono2;
+          usleep(200000);
+          continue;
+        }
+
+      /* If delta_diff is positive and large, gettimeofday reported
+       * more time than actually passed. This could indicate a bug.
+       */
+
+      if (delta_diff > MAX_DELTA_DIFF_US)
+        {
+          fail_msg("FAIL, gettimeofday delta (%lld us) exceeds "
+                   "CLOCK_MONOTONIC delta (%lld us) by %lld us\n",
+                   (long long)tv_delta, (long long)mono_delta,
+                   (long long)delta_diff);
         }
 
       usleep(200000);
       tv1 = tv2;
+      mono1 = mono2;
     }
 }
