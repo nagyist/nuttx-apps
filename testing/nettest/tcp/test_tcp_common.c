zs39/openvela-apps

apps/testing: Add a cmocka framework for network testing
Provides a network automated testing framework, including the main directory structure and Makefile, CMakeLists, Kconfig and other files such as tcp.

Change-Id: Iecfd340d0a1b4c3a214047480daa9165c9f2df5d
Signed-off-by: zhangshuai39 <zhangshuai39@xiaomi.com>

testing/nettest/tcp/test_tcp_common.c
@@ -1,0 +1,47 @@
+/****************************************************************************
+ * apps/testing/nettest/tcp/test_tcp_common.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include "test_tcp.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: tcp_group_setup
+ ****************************************************************************/
+
+int tcp_group_setup(FAR void **state)
+{
+  return 0;
+}
+
+/****************************************************************************
+ * Name: tcp_group_teardown
+ ****************************************************************************/
+
+int tcp_group_teardown(FAR void **state)
+{
+  return 0;
+}

nagyist/nuttx-apps

testing/nettest/tcp/test_tcp_common.c
@@ -1,0 +1,47 @@
+/****************************************************************************
+ * apps/testing/nettest/tcp/test_tcp_common.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include "test_tcp.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: test_tcp_group_setup
+ ****************************************************************************/
+
+int test_tcp_group_setup(FAR void **state)
+{
+  return 0;
+}
+
+/****************************************************************************
+ * Name: test_tcp_group_teardown
+ ****************************************************************************/
+
+int test_tcp_group_teardown(FAR void **state)
+{
+  return 0;
+}